b9b2f6b9d2398f11bd802c6416c9d416e7952932
seu <seumanto@gmail.com>
2018-11-05 12:04:49 +0100

    small changes

--- Key-Distribution/keydistribution.py
+++ Key-Distribution/keydistribution.py
@@ -1,5 +1,6 @@
 from Crypto.PublicKey import RSA
 from Crypto.Signature import PKCS1_PSS
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Hash import SHA256
 
 # Generate key pairs which can be used for signing and verifying
@@ -32,6 +33,34 @@ def generate_keys(n):
 
     return keys_list
 
+def sign_message(private_key, message):
+    h = SHA256.new(message.encode("utf8"))
+    signer = PKCS1_PSS.new(private_key)
+    signature = signer.sign(h)
+
+    return signature
+
+def verify_signature(public_key, signature, h):
+    verifier = PKCS1_PSS.new(public_key)
+    if verifier.verify(h, signature):
+        print("Signature valid :)")
+        return True
+    else:
+        print("Signature invalid :(")
+        return False
+
+def encrypt_message(public_key, message):
+    cipher = PKCS1_OAEP.new(public_key, SHA256)
+    ciphertext = cipher.encrypt(message)
+
+    return ciphertext
+
+def decrypt_message(private_key, ciphertext):
+    cipher = PKCS1_OAEP.new(private_key, SHA256)
+    plaintext = cipher.decrypt(ciphertext)
+
+    return plaintext
+
 # The idea is to let the client generate his own public/private key-pair.
 # Client will send his public key to the Consultant so that
 # the Consultant can encrypt the generated public/private key-pair for the Client,
@@ -40,19 +69,19 @@ def generate_keys(n):
 def distribute(keys_tuple, client_pk):
     pass
 
-# Testing the generated keys for signing and verifying
-private_key, public_key = generate_keypair()
-# _, _= generate_keypair()
-# private_key = RSA.importKey(open('private_key.pem', 'r').read())
-# public_key = RSA.importKey(open('public_key.pem', 'r').read())
-
-message = 'I need to sign this.'
-h = SHA256.new(message.encode("utf8"))
-signer = PKCS1_PSS.new(private_key)
-signature = signer.sign(h)
-
-verifier = PKCS1_PSS.new(public_key)
-if verifier.verify(h, signature):
-    print("Signature valid :)")
-else:
-    print("Signature invalid :(")
+# # Testing the generated keys for signing and verifying
+# private_key, public_key = generate_keypair()
+# # _, _= generate_keypair()
+# # private_key = RSA.importKey(open('private_key.pem', 'r').read())
+# # public_key = RSA.importKey(open('public_key.pem', 'r').read())
+#
+# message = 'I need to sign this.'
+# h = SHA256.new(message.encode("utf8"))
+# signer = PKCS1_PSS.new(private_key)
+# signature = signer.sign(h)
+#
+# verifier = PKCS1_PSS.new(public_key)
+# if verifier.verify(h, signature):
+#     print("Signature valid :)")
+# else:
+#     print("Signature invalid :(")
